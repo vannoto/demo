400ebbc3424aa77972b90cb00b721411332ff624
Vanessa Astronoto <vastronoto.ah@gmail.com>
2018-09-08 23:22:54 -0500

    game update

---
+++ state2.js
@@ -0,0 +1,8 @@
+demo.state2 = function (){};
+demo.state2.prototype = {
+    preload: function (){},
+    create: function (){
+        game.stage.backgroundColor = '#caf0f7'
+    },
+    update: function (){}  
+};
